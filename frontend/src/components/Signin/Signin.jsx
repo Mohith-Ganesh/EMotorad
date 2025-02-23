47ebca7d4e577de989ecccf98b3d434468fecf1b
Mohith Ganesh <98941802+Mohith-Ganesh@users.noreply.github.com>
2025-02-23 22:23:09 +0530

    Update Signin.jsx

--- frontend/src/components/Signin/Signin.jsx
+++ frontend/src/components/Signin/Signin.jsx
@@ -26,7 +26,7 @@ const Signin = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signin", {
+      const response = await fetch("https://e-motorad-hlr8.vercel.app/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
